7da0e40f186755eee6fb44f9b6db85e57bf7c186
Jeff Kile <21stcenturydigitaljeff@gmail.com>
2023-06-07 17:01:39 -0700

    Added gradio web app

---
+++ lesson-1-and-2/app.py
@@ -0,0 +1,16 @@
+from fastai.vision.all import *
+import gradio as gr
+
+learn_inf = load_learner('export.pkl')
+
+categories = ('Car', 'Truck')
+
+def classify_image(img):
+  pred,pred_idx,probs = learn_inf.predict(img)
+  return dict(zip(categories, map(float,probs)))
+
+image = gr.inputs.Image(shape=(224,224))
+label = gr.outputs.Label()
+examples = ['truck.jpg', 'car.jpg', 'plane.jpg']
+
+intf = gr.Interface(fn=classify_image, inputs=image, outputs=label, examples=examples)
